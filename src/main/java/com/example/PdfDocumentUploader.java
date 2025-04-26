dc28dc4c2928a13b525d1abada8194172bb37a7c
appanikaushik <kaushikappani@gmail.com>
2025-04-26 20:44:36 +0530

    Pratice Adaptor Patther

--- src/main/java/com/example/PdfDocumentUploader.java
+++ src/main/java/com/example/PdfDocumentUploader.java
@@ -2,7 +2,6 @@ package com.example;
 
 public class PdfDocumentUploader implements DocumentUploader {
 
-
     @Override
     public void upload() {
         System.out.println("Preparing PDF document...");
---
+++ src/main/java/com/example/WordDocumernAdapator.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class WordDocumernAdapator extends WordDocumentUploader implements DocumentUploader {
+
+  @Override
+  public void upload() {
+    uploadWordDocument();
+  }
+
+}
